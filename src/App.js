5c7adabf709b59f8275df60d36a3922dcbd7f8b8
ivymarkwell <me@ivymarkwell.com>
2020-04-28 20:54:04 -0500

    basic tootie stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Image, Nav, Navbar } from "react-bootstrap";
+import tootie from "./images/tootie.png";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="app">
+      <Navbar className="gray-background">
+        <Navbar.Brand>Tootie</Navbar.Brand>
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#getting-started">Getting Started</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="center">
+        <Image src={tootie} />
+        <p>Tootie is learning to code through the file structure of an app!</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
