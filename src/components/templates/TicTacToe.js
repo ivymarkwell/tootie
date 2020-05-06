226a61af6b9882b1f9ea67b070a96d9809b653ed
ivymarkwell <me@ivymarkwell.com>
2020-05-06 17:22:04 -0500

    use listgroup instead, on click open and closes folders

--- src/components/templates/TicTacToe.js
+++ src/components/templates/TicTacToe.js
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
-import { Card, Dropdown } from "react-bootstrap";
-import CustomMenu from "../materials/CustomMenu";
-import CustomToggle from "../materials/CustomToggle";
+import { Card, ListGroup } from "react-bootstrap";
 import "../../App.css";
 
 function TicTacToe() {
   const [publicFolderOpen, setPublicFolderOpen] = useState(false);
   const [srcFolderOpen, setSrcFolderOpen] = useState(false);
 
-  const publicDiv =
-    "<div id='public-folder-files' style='margin-left: 20px;'><div>favicon.ico</div><div>index.html</div><div>manifest.json</div></div>";
-  const srcDiv =
-    "<div id='src-folder-files' style='margin-left: 20px;'><div>index.css</div><div>index.js</div></div>";
   return (
     <div style={{ height: "93%" }}>
       <Card className="instructions">
@@ -24,66 +18,62 @@ function TicTacToe() {
         <Card className="sidebar">
           <Card.Body>
             <Card.Title>Files</Card.Title>
-            <div id="public-folder" style={{ marginBottom: "16px" }}>
-              <Dropdown
-                as={CustomToggle(
-                  "public-folder-files",
-                  publicFolderOpen,
-                  setPublicFolderOpen
-                )}
+            <ListGroup defaultActiveKey="#link1">
+              <ListGroup.Item
+                action
+                onClick={() => setPublicFolderOpen(!publicFolderOpen)}
+                style={{ all: "unset", display: "flex" }}
               >
-                <Dropdown.Toggle
-                  style={{
-                    backgroundColor: "#ffffff",
-                    border: "none",
-                    color: "black",
-                    paddingBottom: 0,
-                    paddingLeft: 0
-                  }}
-                >
-                  public
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu
-                  as={CustomMenu(
-                    publicDiv,
-                    "public-folder-files",
-                    "public-folder",
-                    publicFolderOpen
-                  )}
-                />
-              </Dropdown>
-            </div>
-            <div id="src-folder" style={{ marginBottom: "16px" }}>
-              <Dropdown
-                as={CustomToggle(
-                  "src-folder-files",
-                  srcFolderOpen,
-                  setSrcFolderOpen
-                )}
+                public <div className="arrow-down" />
+              </ListGroup.Item>
+              {publicFolderOpen && (
+                <>
+                  <ListGroup.Item
+                    action
+                    style={{ all: "unset", display: "flex" }}
+                  >
+                    favicon.ico
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    action
+                    style={{ all: "unset", display: "flex" }}
+                  >
+                    index.html
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    action
+                    style={{ all: "unset", display: "flex" }}
+                  >
+                    manifest.json
+                  </ListGroup.Item>
+                </>
+              )}
+            </ListGroup>
+            <ListGroup defaultActiveKey="#link1">
+              <ListGroup.Item
+                action
+                onClick={() => setSrcFolderOpen(!srcFolderOpen)}
+                style={{ all: "unset", display: "flex" }}
               >
-                <Dropdown.Toggle
-                  style={{
-                    backgroundColor: "#ffffff",
-                    border: "none",
-                    color: "black",
-                    paddingBottom: 0,
-                    paddingLeft: 0
-                  }}
-                >
-                  src
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu
-                  as={CustomMenu(
-                    srcDiv,
-                    "src-folder-files",
-                    "src-folder",
-                    srcFolderOpen
-                  )}
-                />
-              </Dropdown>
-            </div>
+                src <div className="arrow-down" />
+              </ListGroup.Item>
+              {srcFolderOpen && (
+                <>
+                  <ListGroup.Item
+                    action
+                    style={{ all: "unset", display: "flex" }}
+                  >
+                    index.css
+                  </ListGroup.Item>
+                  <ListGroup.Item
+                    action
+                    style={{ all: "unset", display: "flex" }}
+                  >
+                    index.js
+                  </ListGroup.Item>
+                </>
+              )}
+            </ListGroup>
             <Card.Text>.gitignore</Card.Text>
             <Card.Text>README.md</Card.Text>
             <Card.Text>package-lock.json</Card.Text>
